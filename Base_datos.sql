05b8c59af15108546a0c75f3d1e40dffbe703df6
Yeison Betancur <147186691+yBetancurr4002@users.noreply.github.com>
2024-03-18 20:48:14 -0500

    Ajustes Modelo base de datos
    Tablas Estados y Detalles de facturas agregadas. Correspondientes ajustes a la lógica de otras tablas

--- Base_datos.sql
+++ Base_datos.sql
@@ -12,27 +12,32 @@ CREATE TABLE Personas (
     Nombres NVARCHAR(100) NOT NULL,
     Telefono NVARCHAR(20) NOT NULL,
     CorreoElectronico NVARCHAR(100) NOT NULL,
-    DocumentoIdentificacion NVARCHAR(20) NOT NULL
+    DocumentoIdentificacion NVARCHAR(20) NOT NULL,
+    FechaDeCreación DATETIME DEFAULT GETDATE()
 );
 
 CREATE TABLE Usuarios (
     Id INT PRIMARY KEY IDENTITY(1,1),
     PersonaId INT FOREIGN KEY REFERENCES Personas(Id),
     NombreUsuario NVARCHAR(50) NOT NULL,
-    Contrasena NVARCHAR(100) NOT NULL
+    Contrasena NVARCHAR(100) NOT NULL,
+    FechaDeCreación DATETIME DEFAULT GETDATE()
 );
 
 CREATE TABLE Servicios (
     Id INT PRIMARY KEY IDENTITY(1,1),
     Nombre NVARCHAR(100) NOT NULL,
     Descripcion NVARCHAR(255),
-    Precio DECIMAL(10, 2) NOT NULL
+    Precio DECIMAL(10, 2) NOT NULL,
+    FechaDeCreación DATETIME DEFAULT GETDATE()
 );
 
 CREATE TABLE MediosDePago (
     Id INT PRIMARY KEY IDENTITY(1,1),
     Nombre NVARCHAR(100) NOT NULL,
-    Descripcion NVARCHAR(255)
+    Descripcion NVARCHAR(255),
+    FechaDeCreación DATETIME DEFAULT GETDATE()
+
 );
 
 CREATE TABLE Roles (
@@ -40,7 +45,7 @@ CREATE TABLE Roles (
     Nombre VARCHAR(25),
     Descripcion VARCHAR(100),
     Activo BIT DEFAULT 0
-
+    FechaDeCreación DATETIME DEFAULT GETDATE()
 );
 
 CREATE TABLE Clientes (
@@ -50,17 +55,34 @@ CREATE TABLE Clientes (
     FechaDeCreación DATETIME DEFAULT GETDATE()
 );
 
+CREATE TABLE Estados (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Nombre VARCHAR(25),
+    Descripcion VARCHAR(255),
+);
+
 CREATE TABLE Facturas (
     Id INT PRIMARY KEY IDENTITY(1,1),
+    Saldo DECIMAL(10, 2) DEFAULT 0,
     ClienteId INT FOREIGN KEY REFERENCES Clientes(Id),
     ServicioAdquiridoId INT FOREIGN KEY REFERENCES Servicios(Id),
     MedioDePagoId INT FOREIGN KEY REFERENCES MediosDePago(Id),
-	   PagoId INT FOREIGN KEY REFERENCES Pagos(Id),
+	PagoId INT FOREIGN KEY REFERENCES Pagos(Id),
+    EstadoId INT FOREIGN KEY REFERENCES Estados(Id),
+    FechaDeCreación DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE DetallesDeFacturas (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    FacturaId INT FOREIGN KEY REFERENCES Facturas(Id),
+    ServicioId INT FOREIGN KEY REFERENCES Servicios(Id),
     FechaDeCreación DATETIME DEFAULT GETDATE()
 );
 
 CREATE TABLE Pagos (
 	Id INT PRIMARY KEY IDENTITY(1,1),
-	Monto DECIMAL(10, 2) DEFAULT 0,
-	Estado BIT DEFAULT 0
+	MontoDePago DECIMAL(10, 2) DEFAULT 0,
+    FacturaId INT FOREIGN KEY REFERENCES Facturas(Id),
+	Activo BIT DEFAULT 1,
+    FechaDeCreación DATETIME DEFAULT GETDATE()
 );
